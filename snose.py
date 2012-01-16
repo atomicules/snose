56f2b5ba2a529c1beca62adc58fcb28d1f4618ce
atomicules <atomicules@lavabit.com>
2012-01-16 16:07:44 +0000

    Add in remaining basic functionality:
    - Sneeze: Write an existing file out
    - Sniff: Import an existing file as mapping only (doesn't create
      new note on Simplenote)
    - Snot: Think long and dribbly. List of all existing snose synched
      notes
    
    Still lots of improvements to do, such as pulling out repeated
    stuff into functions. Oh, and all the error handling.
    
    But the basics are there.)

--- snose.py
+++ snose.py
@@ -9,7 +9,10 @@ from optparse import OptionParser
 def main():
 	parser = OptionParser()
 	parser.add_option("--snort", action="store", type="string", help="Import a new file to Simplenote")
+	parser.add_option("--sniff", action="store", nargs=2, type="string", help="Import an existing file to Simplenote", metavar="<key> <filename>")
+	parser.add_option("--sneeze", action="store", nargs=2, type="string", help="Export an existing file from Simplenote", metavar="<key> <filename>")
 	parser.add_option("--sync", help="Sync files in index", default=False, action='store_true')
+	parser.add_option("--snot", help="List available notes", default=False, action='store_true')
 	parser.add_option("--username", action="store", type="string", help="Your Simplenote email address")
 	parser.add_option("--password", action="store", type="string", help="Your Simplenote password")
 	(options, args) = parser.parse_args()
@@ -27,6 +30,12 @@ def main():
 	snclient = Simplenote(options.username, options.password)
 	if options.snort:
 		snort(snclient, options.snort)
+	elif options.sniff:
+		sniff(snclient, options.sniff[0], options.sniff[1])
+	elif options.sneeze:
+		sneeze(snclient, options.sneeze[0], options.sneeze[1])
+	elif options.snot:
+		snot(snclient)
 	elif options.sync:
 		sync(snclient)
 	else:
@@ -48,7 +57,7 @@ def snort(snclient,filename):
 	try:
 		with open(filename, 'r') as f:
 			content = f.read()
-		returned = snclient.add_note({"content": content})
+		returned = snclient.add_note({"content": content, "tags": ["snose"]})
 		#Add mapping
 		snose[filename] = {'key': returned[0]['key'], 'syncnum': returned[0]['syncnum'], 'version': returned[0]['version'], 'modifydate': returned[0]['modifydate'] }
 		#Write back out
@@ -56,6 +65,68 @@ def snort(snclient,filename):
 			json.dump(snose, f, indent=2)
 	except IOError as e:
 		pass
+
+def sniff(snclient,key, filename): #How to ensure remote gets or has snose tag?
+	# Add a new mapping only
+	try: #http://stackoverflow.com/questions/82831/how-do-i-check-if-a-file-exists-using-python#85237
+		with open('.snose', 'r') as f:
+			snose = json.load(f)
+	except IOError as e:
+		#Doesn't exist so create new
+		snose = {}
+	#Get details about current Simplenote file
+	remote = snclient.get_note(key)
+	#What if can't be found, need to abort...
+	try:
+		#Add mapping
+		snose[filename] = {'key': remote[0]['key'], 'syncnum': remote[0]['syncnum'], 'version': remote[0]['version'], 'modifydate': remote[0]['modifydate'] }
+		#Write back out
+		with open('.snose', 'w') as f:
+			json.dump(snose, f, indent=2)
+	except IOError as e:
+		pass
+
+def sneeze(snclient, key, filename):
+	#place an existing note in current directory
+	#Need some UI for getting list of keys
+	#Get remote note
+	remote = snclient.get_note(key)
+	#Add mapping
+	try:
+		with open('.snose', 'r') as f:
+			snose = json.load(f)
+	except IOError as e:
+		#Doesn't exist so create new
+		snose = {}
+	remote = snclient.get_note(key)	
+	try:
+		snose[filename] = {'key': remote[0]['key'], 'syncnum': remote[0]['syncnum'], 'version': remote[0]['version'], 'modifydate': remote[0]['modifydate'] }
+		#Write back out
+		with open('.snose', 'w') as f:
+			json.dump(snose, f, indent=2)
+	except IOError as e:
+		pass
+	#Write file itself
+	try: 
+		with open(filename, 'w') as f:
+			f.write(remote[0]['content'])
+	except IOError as e:
+			pass
+	#Now then, only problem with this is that if sync is called, this will be "modified" and will get synced unnecessarily
+	#Not sure how to get around this? Override the local['modifydate']?? I think so...
+	#but then need to do that after file has written to disk
+
+def snot(snclient):
+	#List simplenote notes
+	notelist = snclient.get_note_list()
+	#That gets list of keys. Then need to iterate through and get first line of text.
+	#This is going to be slow. Perhaps limit to a tag is a good idea
+	print "Key:                                    Note"
+	for note in notelist[0]:
+		if "snose" in note['tags']:
+			#get note itself
+			remote = snclient.get_note(note['key'])
+			print remote[0]['key']  + "  " + remote[0]['content'].splitlines()[0]
 
 
 def sync(snclient):
@@ -106,9 +177,5 @@ def sync(snclient):
 			except IOError as e:
 				pass
 
-def sneeze(key, filename):
-	#place an existing note in current directory
-	#Need some UI for getting list of keys
-	pass
 
 main()
